0f68f1a8ece2bf5dd10464e76a8ab5e47e34e50f
panchaljay <panchaljay752@gmail.com>
2024-02-25 20:45:04 +0530

    first commit

---
+++ src/Components/Photowall.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Photo from './Photo'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+function Photowall(props) {
+    return (
+        <div>
+            <Link
+                style={{ textDecoration: "none", color: "inherit" }}
+                to="/AddPhoto">
+                <i style={{ marginTop: '0px ' }} class="addIcon fa-duotone fa-plus" ></i>
+            </Link>
+            <div className="photoGride" >
+                {props.posts
+                    .sort(function (x, y) {
+                        return y.id - x.id
+                    })
+                    .map((post, index) => <Photo key={index} post={post} {...props} index={index} />)}
+            </div>
+        </div>
+    )
+
+}
+
+Photowall.propTypes = {
+    posts: PropTypes.array.isRequired,
+
+}
+
+export default Photowall
